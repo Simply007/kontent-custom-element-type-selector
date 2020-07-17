96d039edb2257ff5b7646e4c346e67b7d48230d4
ondrejch <ondrejch@kentico.com>
2020-07-17 14:28:37 +0200

    implement custom element

---
+++ src/components/ContentTypeSelector.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <multiselect
+      v-model="selectedTypes"
+      placeholder="Select types"
+      open-direction="bottom"
+      :options="options"
+      :multiple="true"
+      :loading="isLoading"
+      label="name"
+      track-by="codename"
+      @input="onSelect"
+    >
+      <template slot="singleLabel" slot-scope="{ option }">
+        <strong>{{ option.sys.name }}</strong>
+      </template>
+    </multiselect>
+  </div>
+</template>
+
+<script>
+import Multiselect from "vue-multiselect";
+
+export default {
+  components: {
+    Multiselect
+  },
+  data() {
+    return {
+      selectedTypes: this.value || [],
+      options: [],
+      isLoading: true
+    };
+  },
+  created() {
+    this.fetchTypes();
+  },
+  props: {
+    element: {
+      type: Object,
+      required: true
+    },
+    context: {
+      type: Object,
+      required: true
+    },
+    value: {
+      type: Object
+    }
+  },
+  methods: {
+    limitText(count) {
+      return `and ${count} other countries`;
+    },
+    fetchTypes() {
+      fetch(
+        `https://deliver.kontent.ai/${this.context.projectId}/types?elements=null`
+      )
+        .then(response => response.json())
+        .then(json => {
+          this.options = json.types.map(type => type.system);
+          this.isLoading = false;
+        });
+    },
+    onSelect: function() {
+      this.save(this.selectedTypes);
+    },
+    save: function(value) {
+      this.$emit("update:value", value);
+    }
+  }
+};
+</script>
+
+<style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
+
+<style scoped>
+</style>
